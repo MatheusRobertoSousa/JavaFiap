b6d5147560aba55ff143206dc0218116d2717b34
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-01 10:56:11 -0300

    Corrige o bug do metodo empate e vitoria

--- src/br/com/fiap/models/Time.java
+++ src/br/com/fiap/models/Time.java
@@ -1,5 +1,33 @@
 package br.com.fiap.models;
 
 public class Time {
+	
+private String nome;
+	
+	private int pontos;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(int pontos) {
+		this.pontos = pontos;
+	}
+	
+	public void addVitoria() {
+		pontos = pontos + 3;
+	}
+	
+	public void addEmpate() {
+		pontos++;
+	}
 
 }
